36be8ec0d7e1df40067f09db1c831b5425b88ef6
Mary Webby <118230483+marywebby@users.noreply.github.com>
2024-02-14 22:27:31 +0000

    user details loooks good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,21 @@ class UsersController < ApplicationController
     matching_users = User.all
 
     @list_of_users = matching_users.order({ :username => :asc })
-    
+
     render({ :template => "user_templates/index"})
+  end 
+
+  def show 
+    input_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username => input_username })
+
+    @the_user = matching_usernames.first
+
+    if @the_user == nil
+      redirect_to("/users", status: 404)
+    else 
+      render({ :template => "user_templates/show"})
+    end 
   end 
 end
