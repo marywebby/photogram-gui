7846725f44b937c409e2fd1468dd3b82b56a62b5
Mary Webby <118230483+marywebby@users.noreply.github.com>
2024-02-14 22:57:23 +0000

    added some logic for photos, ended when creating the inserting photo record

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,10 @@
+class PhotosController < ApplicationController
+  def index 
+
+    render({ :template => "photo_templates/index"})
+  end 
+
+  def create
+    
+  end 
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   get("/users/:path_username", { :controller => "users", :action => "show" })
 
+  get("/photos", { :controller => "photos", :action => "index"})
+
+  get("/photos/insert_photo_record", { :controller => "photos", :action => "create"})
+
 end
